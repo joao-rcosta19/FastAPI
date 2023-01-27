40b5c5473a98fb1420cbc24d6c16e0e2110d1516
joao-rcosta19 <joao.rcosta19@gmail.com>
2023-01-26 21:24:11 -0300

    atualização

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 from api.infra.config.database import buscar, atualizaSitEmail, conectar, close, inserindoEmail, buscarsituacao
@@ -13,10 +13,44 @@ class Item(BaseModel):
     corpo: str
 
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 
 @app.post("/items/")
 async def create_item(item: Item):
     validar = buscar(item.chave, "instituicao", "chave_toker")
-    return validar
+    if validar == False:
+        return json.dumps({'status': 'Nao Enviado! verifique sua chave!', 'chave inserida': item.chave})
+    else:
+        frase = " "
+        if validaCampo(item.email) == False:
+            return json.dumps({'status': 'Nao Enviado! verifique se o campo endereco de email esta vazio!', 'endereco': item.email})
+        elif frase not in item.email:
+            a = buscar(item.email, "lista_email2", "email")
+        else:
+            teste = item.email.replace(" ","")
+            a = buscar(teste, "lista_email2", "email")
+            item.email = teste
+        #inserindo e validando email
+        if a != True:
+            inserindoEmail(item.email, "lista_email2", item.chave)
+        vEmail = validaEmail(item.email) #acrecentar codigo de validação
+        if vEmail == True: 
+            if buscarsituacao(item.email, "lista_email2", "email", 2) == False:
+                atualizaSitEmail("lista_email2", 2, item.email) #para validos 
+        else:
+            if buscarsituacao(item.email, "lista_email2", "email", 1) == False:
+                atualizaSitEmail("lista_email2", 1, item.email) #para invalidos
+         #enviar email
+        if validaCampo(item.assunto) == True and validaCampo(item.corpo) == True:
+            if buscarsituacao(item.email, "lista_email2", "email", 2) == True:
+                enviaremail(item.assunto, item.corpo, item.email)
+                return json.dumps({'chave': item.chave, 'assunto do email': item.assunto, 'corpo do email': item.corpo, 'destinatario': item.email, 'status': 'Enviado!'})
+            else:
+                return json.dumps({'status': 'Nao Enviado! verifique o endereco de email...', 'destinatario': item.email})
+        elif validaCampo(item.assunto) == False:
+            return json.dumps({'status': 'Nao Enviado! verifique se o campo Assunto esta vazio!', 'assunto do email': item.assunto})
+        elif validaCampo(item.corpo) == False:
+            return json.dumps({'status': 'Nao Enviado! verifique se o campo Corpo do email esta vazio!', 'corpo do email': item.corpo})
+        else:
+            return json.dumps({'status': 'Nao Enviado! verifique se o campo Assunto e Corpo do email estao vazio!', 'assunto do email': item.assunto , 'corpo do email': item.corpo})
